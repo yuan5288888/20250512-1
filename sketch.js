0db78489e7d5b4a9c5c743771f29d8f811a07abc
yuan5288888 <vivianyuan950528@gmail.com>
2025-05-12 11:01:17 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+let facemesh;
+let video;
+let predictions = [];
+const pointsToConnect = [
+  409, 270, 269, 267, 0, 37, 39, 40, 185, 61, 146, 91, 181, 84, 17, 314, 405, 321, 
+  375, 291, 76, 77, 90, 180, 85, 16, 315, 404, 320, 307, 306, 408, 304, 303, 302, 
+  11, 72, 73, 74, 184
+];
+
+function setup() {
+  createCanvas(400, 400);
+  video = createCapture(VIDEO);
+  video.size(width, height);
+  video.hide();
+
+  facemesh = ml5.facemesh(video, modelReady);
+  facemesh.on("predict", results => {
+    predictions = results;
+  });
+}
+
+function modelReady() {
+  console.log("Facemesh model loaded!");
+}
+
+function draw() {
+  image(video, 0, 0, width, height);
+  drawFacemesh();
+}
+
+function drawFacemesh() {
+  if (predictions.length > 0) {
+    const keypoints = predictions[0].scaledMesh;
+
+    stroke(255, 0, 0); // 紅色線條
+    strokeWeight(5); // 線條粗細為 5
+
+    // 使用 line 指令將所有點串接在一起
+    for (let i = 0; i < pointsToConnect.length - 1; i++) {
+      const [x1, y1] = keypoints[pointsToConnect[i]];
+      const [x2, y2] = keypoints[pointsToConnect[i + 1]];
+      line(x1, y1, x2, y2);
+    }
+
+    // 將最後一個點與第一個點連接，形成閉合的形狀
+    const [xStart, yStart] = keypoints[pointsToConnect[0]];
+    const [xEnd, yEnd] = keypoints[pointsToConnect[pointsToConnect.length - 1]];
+    line(xEnd, yEnd, xStart, yStart);
+  }
+}
